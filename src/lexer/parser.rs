8711351662e5168f1244e84c40579ddb05be5431
kernel-dev <twitchisadeck@gmail.com>
2022-11-26 05:03:27 +0100

    refactor(Lexer): implement function for resolving file paths

--- src/lexer/parser.rs
+++ src/lexer/parser.rs
@@ -11,33 +11,46 @@ use super::validator::LexerValidator;
 
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-pub fn parse_example() {
-    let file_name = "example.xbs";
+pub struct ResolvedEntry {
+    pub file_name: String,
+    pub full_path: String,
+    pub parent_directory: String,
+}
 
-    let mut cwd = env::current_dir().ok().unwrap();
+pub struct Parser {}
 
-    while (cwd
-        .to_str()
-        .unwrap()
-        .split(if env::consts::OS == "windows" {
-            '\\'
-        } else {
-            '/'
-        })
-        .last()
-        .unwrap()
-        != "XBS")
-    {
-        cwd.pop();
+impl Parser {
+    pub fn resolve_entry(cli_args: env::Args) -> ResolvedEntry {
+        let mut path: PathBuf = PathBuf::new();
+
+        for arg in cli_args {
+            if !arg.contains(".xbs") {
+                continue;
+            }
+
+            if Path::new(&arg).exists() {
+                path = fs::canonicalize(PathBuf::from(arg)).unwrap();
+                break;
+            }
+        }
+
+        //
+        //  This is very unsafe, should
+        //  implement a better way of handling these
+        //  conversions.
+        //
+        ResolvedEntry {
+            full_path: path.to_str().unwrap().to_string(),
+            file_name: path.file_name().unwrap().to_str().unwrap().to_string(),
+            parent_directory: path.parent().unwrap().to_str().unwrap().to_string(),
+        }
     }
 
-    cwd.push("example.xbs");
+    pub fn parse(entry: ResolvedEntry) -> () {
+        let contents = fs::read_to_string(entry.full_path).unwrap();
 
-    // println!("Complete path: {}", cwd.to_str().unwrap());
-
-    let contents = fs::read_to_string(cwd).expect("Failed to read contents of Example.xbs!");
-
-    println!("Contents: \n{contents}");
+        println!("{contents}");
+    }
 }
